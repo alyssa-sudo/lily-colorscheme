c99c8c8a99a50c188be224d4fbb23c72e82e7120
addy-dclxvi <addy4rock@gmail.com>
2021-11-24 21:35:42 -0500

    Add Alacritty, Kitty, and Wezterm.

---
+++ wezterm.lua
@@ -0,0 +1,23 @@
+local colors = {
+	bg = "#2b2839",
+	fg = "#d5d1eb",
+	black = "#2b2839",
+	red = "#d7757d",
+	green = "#97baa3",
+	yellow = "#C0A496",
+	blue = "#92ade3",
+	magenta = "#ab85d1",
+	cyan = "#C28EBE",
+	white = "#d5d1eb",
+	cursor = "#eeedf7",
+}
+local colors_f = {
+	colors.black,
+	colors.red,
+	colors.green,
+	colors.yellow,
+	colors.blue,
+	colors.magenta,
+	colors.cyan,
+	colors.white,
+}
